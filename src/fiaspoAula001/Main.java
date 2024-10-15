3b43c5eb82f41406eee4c0cef136bf46de885d77
joao victor benedet machado <jvbm2177@gmail.com>
2024-10-14 23:21:24 -0300

    api teste

---
+++ src/fiaspoAula001/Main.java
@@ -0,0 +1,20 @@
+package fiaspoAula001;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Tipos Primitivos //
+
+        int idade = 2145483647; // número máximo de um INT é 2,147,483,647 // 32bits
+        double altura = 1.92;
+        char letra = 'a';
+        Long numerosGrandes = 2145483648L; // 64bits
+
+        // Tipos Não Primitivos //
+
+        // são tipos de dados que podem receber atributos, por exemplo: //
+        String frase = "Ola meu nome é joao";
+        char quartaLetra = frase.charAt(3);
+
+    }
+}
